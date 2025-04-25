f61485567f6a4c29fa40a04090342fcf85b04a36
Mikebrown0409 <mikebrown0409@gmail.com>
2025-04-25 16:02:26 -0700

    Change html ships to buttons. Add cell class to main.css. add true constants and cell related data, add gamestate for state variables to track for runtime, fix createEmptyGrid.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
   /*----- constants -----*/
+  const GRID_SIZE = 10;
 
+  //length refers to cell length once we have inside grid created aka cells.
+  const SHIP_TYPES = {
+    carrier: { name: 'Carrier', length: 5 },
+    battleship: { name: 'Battleship', length: 4 },
+    cruiser: { name: 'Cruiser', length: 3 },
+    submarine: { name: 'Submarine', length: 3 },
+    destroyer: { name: 'Destroyer', length: 2 },
+  };
+
+  // cell types inside grid should cover all things that will happen.
+  //TODO map these somehow once inside grid is created
+  const CELL_TYPES = {
+    Water: 0,
+    Ship: 1,
+    Hit: 2,
+    Miss: 3,
+    Sunk: 4,
+  }
+  
 
   /*----- state variables -----*/
 
@@ -11,6 +31,7 @@
 
   let turn;
   let winner;
+  let gameState;
   
   let shipSelected;
   let shipsToPlace;
@@ -18,6 +39,8 @@
 
   /*----- cached elements  -----*/
 
+    const playerGridEl = document.querySelector('#player-grid');
+    const enemyGridEl = document.querySelector('#enemy-grid'); 
 
   /*----- event listeners -----*/
 
@@ -29,16 +52,33 @@
     playerGrid = createEmptyGrid();
     enemyGrid = createEmptyGrid();
   
-    playerShips = initializeShipObjects();
-    enemyShips = initializeShipObjects();
+    // playerShips = initializeShipObjects();
+    // enemyShips = initializeShipObjects();
   
     winner = null;
     turn = 'Player';
+    gamestate = 'placeholder'
 
     shipSelected = null;
     //need to figure out shipsToPlace
+
     render();
   }
+
+  //resolved errors. Should be good, nested row and then adds water value which we have at 0 to each nested row when iterating through it. 
+  // then adds row to grid. Should be 10by10 rows of '0'.
+  // circle back if issue once the rest of grid is set up.  
+  function createEmptyGrid() {
+    const grid = [];
+    for (let r = 0; r < GRID_SIZE; r++) {
+        const row = [];
+        for (let c = 0; c < GRID_SIZE; c++) {
+            row.push(CELL_TYPES.Water); 
+        }
+        grid.push(row);
+    }
+    return grid;
+}
 
   function render () {
     renderGrids();
@@ -47,18 +87,26 @@
   };
 
   function renderGrids() {
-    renderPlayerGrid(playerGrid,playerShips);
-    renderEnemyGrid(enemyGrid,enemyShips);
+    renderSingleGrid(playerGridEl, playerGrid, 'p', true);
+    renderSingleGrid(playerGridEl, playerGrid, 'e', false);
   }
 
-  function renderPlayerGrid() {
+  function renderSingleGrid(gridElement, gridData, prefix, showShips) {
+    for (let r = 0; r < GRID_SIZE; r++) {
+        for (let c = 0; c < GRID_SIZE; c++) {
+            // Select the cell ..*Doesnt* currently work.. need to fix. We have all 0's in our grid but no column,row array set. think data.
+            const cellEl = gridElement.querySelector(`#${gridData}-r${r}c${c}`);
 
-  }
+            const state = gridData[r][c]; 
+        }
+    }
+}
+
 
   function renderEnemyGrid() {
 
   }
-  
+
   function renderMessage() {
 
   }
@@ -66,3 +114,5 @@
   function renderControls() {
 
   }
+
+  
